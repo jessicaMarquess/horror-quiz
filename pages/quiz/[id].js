9f25b4b8b40ad08016daa41c3657e37ab58e942e
Jessica Marques <jessica.marques.dev@gmail.com>
2021-01-31 18:36:35 -0300

    quarto dia

--- pages/quiz/[id].js
+++ pages/quiz/[id].js
@@ -1,10 +1,10 @@
+/* eslint-disable linebreak-style */
 import React from 'react';
 
-
-export default fucntion QuizDaGaleraPage() {
-    return (
-        <div>
-            Até logo!
-        </div>
-    );  
+export default function QuizDaGaleraPage() {
+  return (
+    <div>
+      Até logo!
+    </div>
+  );
 }
